d9d2548ca5fecae4108ea6296f2bc22a74af9a79
Michel <67877897+Catmandoline@users.noreply.github.com>
2024-08-05 21:22:39 +0200

    Basic Project Setup

---
+++ Space Invaders Homage/Space Invaders Homage/Space_Invaders_HomageApp.swift
@@ -0,0 +1,17 @@
+//
+//  Space_Invaders_HomageApp.swift
+//  Space Invaders Homage
+//
+//  Created by Michel Matys on 05.08.24.
+//
+
+import SwiftUI
+
+@main
+struct Space_Invaders_HomageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Space Invaders Homage/Space Invaders HomageUITests/Space_Invaders_HomageUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Space_Invaders_HomageUITestsLaunchTests.swift
+//  Space Invaders HomageUITests
+//
+//  Created by Michel Matys on 05.08.24.
+//
+
+import XCTest
+
+final class Space_Invaders_HomageUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
