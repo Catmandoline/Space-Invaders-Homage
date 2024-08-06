e2004534ed02afc045ac6774eab22bbe13a69717
Michel <michel.ma@gmx.de>
2024-08-06 16:02:20 +0200

    create projectile and move function for ship

--- Space Invaders Homage/Space Invaders Homage/GameScene/GameScene.swift
+++ Space Invaders Homage/Space Invaders Homage/GameScene/GameScene.swift
@@ -13,6 +13,9 @@ class GameScene: SKScene {
     
     let background = SKSpriteNode(imageNamed: "background-space")
     var player = SKSpriteNode(imageNamed: "playerShip")
+    var shipFire = SKSpriteNode()
+    
+    var fireTimer = Timer()
     
     override func didMove(to view: SKView) {
         scene?.size = CGSize(width: 400, height: 800)
@@ -21,6 +24,7 @@ class GameScene: SKScene {
         background.zPosition = 1
         addChild(background)
         makePlayer()
+        fireTimer = .scheduledTimer(timeInterval: 0.7, target: self, selector: #selector(shipFireFunction), userInfo: nil, repeats: true)
     }
     
     func makePlayer() {
@@ -28,7 +32,29 @@ class GameScene: SKScene {
         player.position = CGPoint(x: size.width / 2, y: 100)
         player.zPosition = 10
         addChild(player)
+    }
+    
+    @objc func shipFireFunction() {
+        shipFire = .init(imageNamed: "projectile")
+        shipFire.position = player.position
+        shipFire.zPosition = 2
         
+        addChild(shipFire)
+        
+        let moveAction = SKAction.moveTo(y: 800, duration: 0.7)
+        let deleteAction = SKAction.removeFromParent()
+        let combine = SKAction.sequence([moveAction,deleteAction])
+        
+        shipFire.run(combine)
+        
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            let location = touch.location(in: self)
+            
+            player.position.x = location.x
+        }
     }
 }
 
