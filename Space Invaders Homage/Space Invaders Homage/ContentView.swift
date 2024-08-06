9ff55739a44965f3bee8c14ff72b8c5ea62437e0
reneschwarz <rene.schwarz1989@gmail.com>
2024-08-06 16:53:34 +0200

    WIP ContentView

--- Space Invaders Homage/Space Invaders Homage/ContentView.swift
+++ Space Invaders Homage/Space Invaders Homage/ContentView.swift
@@ -13,32 +13,62 @@ struct ContentView: View {
     @State private var showGameScene = false
     @State private var playerName = ""
     @State private var playersMaxScore = 0
-    
+    @State private var playerLives = 3
+    @State private var playerScore = 0
     
     var body: some View {
         ZStack {
-            Image("background-contenview")
-                .resizable()
-                .scaledToFill()
-                .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
-                .clipped()
-                .edgesIgnoringSafeArea(.all)
+            if !showGameScene {
+                Image("background-contenview")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+                    .clipped()
+                    .edgesIgnoringSafeArea(.all)
+                
+                VStack {
+                    
+                    Spacer()
+                    
+                    Button("Spiel Starten") {
+                        showGameScene = true
+                    }
+                    .padding()
+                    .background(Color.brown)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+                    .shadow(radius: 10)
+                    .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height - 150)
+                }
+            }
             
             if showGameScene {
                 SpriteView(scene: GameScene())
                     .ignoresSafeArea()
-            }
-            
-            VStack {
-                Button("Spiel Starten") {
-                    showGameScene = true
+                HStack {
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(maxWidth: 120, maxHeight: 30)
+                            .foregroundColor(.brown)
+                        Text("Score: \(playerScore)")
+                            .foregroundColor(.white)
+                            .padding()
+                    }
+                    Spacer()
+                    ZStack{
+                        RoundedRectangle(cornerRadius: 10)
+                            .frame(maxWidth: 120, maxHeight: 30)
+                            .foregroundColor(.brown)
+                    
+                        Text("Lives: \(playerLives)")
+                            .foregroundColor(.white)
+                            
+                    }
+                    
                 }
-                .padding()
-                .background(Color.brown)
-                .foregroundColor(.white)
-                .cornerRadius(10)
-                .shadow(radius: 10)
-                .position(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height - 100)
+                .padding(.horizontal, 10)
+                Spacer()
+                
             }
         }
     }
